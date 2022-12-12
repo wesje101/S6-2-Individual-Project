12543f8d46d6a4802739061014fce397b691bd12
Wesley <wesley.verhagen@student.fontys.nl>
2022-12-12 13:37:51 +0100

    Reverted environment variable in apigateway

--- RabbitMQPrototype/APIGateway/Program.cs
+++ RabbitMQPrototype/APIGateway/Program.cs
@@ -2,32 +2,16 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-string? runningEnvironment = Environment.GetEnvironmentVariable("HOSTED_ENVIRONMENT");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-switch (runningEnvironment)
-{
-    case ("docker"):
-        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-            .AddJsonFile("Ocelot.Development.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables();
-        break;
-    case ("kubernetes"):
-        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-            .AddJsonFile("Ocelot.Kubernetes.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables();
-        break;
-    default:
-        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-            .AddJsonFile("Ocelot.Development.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables();
-        break;
-}
-
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
