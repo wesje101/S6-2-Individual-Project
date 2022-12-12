f48fef920b5f2f4dc3735ef0474b3b82713a0b99
Wesley <wesley.verhagen@student.fontys.nl>
2022-12-12 11:29:20 +0100

    kubernetes debugging

---
+++ RabbitMQPrototype/Kubernetes/RebootDeployments.bat
@@ -0,0 +1,5 @@
+kubectl rollout restart deployment s6app-apigateway-deployment
+kubectl rollout restart deployment s6app-authservice-deployment
+kubectl rollout restart deployment s6app-accountservice-deployment
+
+pause
